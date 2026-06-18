3cdc1d8eaa036766853a7d7fe568c1d9852c3293
jinnymoon2 <jinny@extory.co>
2026-06-18 18:00:54 +0900

    update: handle instagram oauth callback on homepage

--- .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
+++ .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0t21ert._.js"],
+    chunks: ["static/chunks/_0732ba-._.js"],
     source: "dynamic"
 });
