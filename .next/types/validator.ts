b4315af805a11499b1300725f8a73bad0767fe5f
jinnymoon2 <jinny@extory.co>
2026-06-18 18:17:38 +0900

    fix: support instagram app environment variables

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -56,6 +56,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../app/api/debug/instagram/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/debug/instagram">> = Specific
+  const handler = {} as typeof import("../../app/api/debug/instagram/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../app/api/instagram/callback/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/instagram/callback">> = Specific
